ca4843d63f41865f6ccd37f4de92b535dc98db66
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-06-11 18:57:32 -0400

    Refactoring

--- src/room.py
+++ src/room.py
@@ -4,3 +4,19 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.n_to = None
+
+    def __str__(self):
+        output = f'{self.name}: {self.description}\n'
+        if self.s_to:
+            output += 'To the south is: ' + self.s_to.name + '\n'
+        if self.e_to:
+            output += 'To the south is: ' + self.s_to.name + '\n'
+        if self.w_to:
+            output += 'To the south is: ' + self.s_to.name + '\n'
+        if self.n_to:
+            output += 'To the south is: ' + self.s_to.name + '\n'
+        return output
