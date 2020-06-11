161e4163e4e96b5ae9df231ea48d8c06311b9d7b
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-06-11 19:09:29 -0400

    Most refactoring done

--- src/adv.py
+++ src/adv.py
@@ -43,10 +43,6 @@ aaron = Player("Aaron", room['outside'])
 
 
 # Write a loop that:
-choice = 'n'
-print('Type (q) to quit')
-print(f'{aaron.name} is currently in {aaron.current_room.name}')
-print(f'{aaron.current_room.description}')
 
 
 def location():
@@ -55,55 +51,17 @@ def location():
 
 
 while True:
+    print('Type (q) to quit')
+    print(f'{aaron.name} is currently in {aaron.current_room.name}')
+    print(f'{aaron.current_room.description}')
+    choice = input('Enter a direction: ')
 
-    if aaron.current_room.name == 'Outside Cave Entrance':
-        choice = input(
-            'Please select (n) to head into the Foyer: ')
-    elif aaron.current_room.name == 'Foyer':
-        choice = input(
-            'Select (n) to venture to the Overlook or select (e) to explore the Narrow. Otherwise select (s) to go back: ')
-    elif aaron.current_room.name == 'Grand Overlook':
-        choice = input(
-            'Please select (s) to head back into the Foyer: ')
-    elif aaron.current_room.name == 'Narrow Passage':
-        choice = input(
-            'Please select (w) to head into the Foyer or select (n) to head to the Treasure room: ')
-    else:
-        choice = input(
-            'Please select (s) to head into the Narrow: ')
+    if choice in {'n', 's', 'e', 'w'}:
+        if hasattr(aaron.current_room, f'{choice}_to'):
+            aaron.current_room = getattr(
+                aaron.current_room, f'{choice}_to')
 
-    try:
-        if (choice == 'q'):
-            break
-        if (aaron.current_room.name == 'Outside Cave Entrance' and choice == 'n'):
-            aaron.current_room = room['foyer']
-            location()
-        elif (aaron.current_room.name == 'Foyer' and choice == 'n'):
-            aaron.current_room = room['overlook']
-            location()
-        elif (aaron.current_room.name == 'Foyer' and choice == 'e'):
-            aaron.current_room = room['narrow']
-            location()
-        elif (aaron.current_room.name == 'Foyer' and choice == 's'):
-            aaron.current_room = room['outside']
-            location()
-        elif (aaron.current_room.name == 'Grand Overlook' and choice == 's'):
-            aaron.current_room = room['foyer']
-            location()
-        elif (aaron.current_room.name == 'Narrow Passage' and choice == 'w'):
-            aaron.current_room = room['foyer']
-            location()
-        elif (aaron.current_room.name == 'Narrow Passage' and choice == 'n'):
-            aaron.current_room = room['treasure']
-            location()
-        elif (aaron.current_room.name == 'Treasure Chamber' and choice == 's'):
-            aaron.current_room = room['narrow']
-            location()
-        else:
-            print('Please enter a valid directional movement')
 
-    except ValueError:
-        print('Please select a valid directional movement')
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
