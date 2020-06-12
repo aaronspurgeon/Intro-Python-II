1a046a5842db04f61eab36538d8451eca96b552a
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-06-11 21:00:37 -0400

    Started on my methods, implementing first as regular functions to make it easier

--- src/adv.py
+++ src/adv.py
@@ -52,12 +52,13 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-aaron = Player("Aaron", room['outside'])
+aaron = Player("Aaron", room['overlook'])
+aaron.items = [torch, compass, coin]
 
 
 # Write a loop that:
 
-
+# functions
 def location():
     print('Type (q) to quit')
     print(f'{aaron.name} is currently in {aaron.current_room.name}')
@@ -65,18 +66,51 @@ def location():
     print(aaron.current_room)
 
 
-while True:
-    location()
-    choice = input('Enter a direction: ')
+def inventory():
+    output = f'This is your current inventory: '
+    for i in aaron.items:
+        output += f' {i.name} '
+    print(output)
 
-    if choice in {'n', 's', 'e', 'w'}:
-        try:
-            hasattr(aaron.current_room, f'{choice}_to')
-            aaron.current_room = getattr(
-                aaron.current_room, f'{choice}_to')
-            location()
-        except AttributeError:
-            print('Please select a valid direction')
+
+def getItem(item):
+    for i in aaron.current_room.items:
+        if i.name == item:
+            aaron.items.append(i)
+            aaron.current_room.items.remove(i)
+
+
+for i in aaron.current_room.items:
+    print(i.name)
+getItem('Flask')
+for i in aaron.current_room.items:
+    print(i.name)
+inventory()
+
+
+def dropItem(item):
+    for i in aaron.items:
+        if i.name == item:
+            aaron.items.remove(i)
+            print(f'{i.name} was removed')
+
+
+dropItem('Coin')
+inventory()
+
+# while True:
+#     location()
+#     choice = input('Enter a direction: ')
+
+#     if choice in {'n', 's', 'e', 'w'}:
+#         try:
+#             hasattr(aaron.current_room, f'{choice}_to')
+#             aaron.current_room = getattr(
+#                 aaron.current_room, f'{choice}_to')
+#         except AttributeError:
+#             print('Please select a valid direction')
+#     if (choice == 'q'):
+#         break
 
 
 #
