822193d568176ac4c6f1dd82bba8bc515beff144
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-06-11 20:25:14 -0400

    Added items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,37 @@
 from room import Room
 from player import Player
+from items import Item
+
+# rooms items
+torch = Item('Torch', 'A simple torch, seems to be losing its burn quickly')
+compass = Item(
+    'Compass', 'Dirt covered compass, seems to have been here awhile.')
+flask = Item(
+    'Flask', 'A jeweled covered flask, there seems to be bit of foul smelling liquid at the bottom')
+helmet = Item(
+    'Helmet', 'A broken helmet, the aged leather gives it an ancient look')
+coin = Item(
+    'Coin', 'A lone gold coin found by the exit. Whoever looted this place left in a hurry.')
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", torch),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", compass),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", flask),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", helmet),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", coin),
 }
 
 
@@ -33,6 +45,7 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+
 
 #
 # Main
@@ -49,6 +62,7 @@ def location():
     print('Type (q) to quit')
     print(f'{aaron.name} is currently in {aaron.current_room.name}')
     print(f'{aaron.current_room.description}')
+    print(f'Current items in room are: {aaron.current_room.items.name}')
 
 
 while True:
--- src/room.py
+++ src/room.py
@@ -1,16 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = items
         self.s_to = None
         self.e_to = None
         self.w_to = None
         self.n_to = None
 
     def __str__(self):
-        output = f'{self.name}: {self.description}\n'
+        output = f'{self.name}: {self.description}\n Items here: {self.items}'
         if self.s_to:
             output += 'To the south is: ' + self.s_to.name + '\n'
         if self.e_to:
